368616674623874b09deb702d64b3c8b22720ce6
pjwilcoxen <wilcoxen@syr.edu>
2018-07-17 19:05:17 -0400

    Revise and extend Elvira.
    The previous version didn't divert the original messages from
    traders that Elvira was spoofing so the target markets ended up
    with extra demand messages. For example, if Elvira was spoofing
    node 191 to market 202, 202 would end up with the original 191
    message as well as the spoofed version.
    
    The issue orginated in Adversary, which set up diversions early
    that were then reset before they were used. In addition, the
    diversions in Adversary were more extensive than needed for
    some subclasses (e.g., divert_to and divert_from when only
    divert_from was actually needed). Remove diversions from
    Adversary and implement them in individual adversaries
    as appropriate.
    
    Also, extended Elvira to attack multiple target markets.
    
    Logging greatly increased, especially when debugging is on.

--- src/Adv_Elvira.java
+++ src/Adv_Elvira.java
@@ -1,42 +1,145 @@
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Arrays;
 import java.util.ArrayList;
 import sim.engine.SimState;
 
 /**
- * Class that extends Adversary.  Implements an malicious
- * actor that sends false bids indiscriminately.  Does 
- * not hide their own identity
+ * Adversary Elvira.
+ * 
+ * Attacks markets with constrained transmission to their upstream
+ * nodes. Forges demand messages but modifying historical demands
+ * sent by one or more compromised nodes. 
+ * 
+ * Markets that were demand constrained get tweaks lowering net demand; 
+ * markets constrained on the supply side get tweaks raising net demand. 
+ * 
+ * Multiple markets may be attacked by listing them in the configuration
+ * file with plus signs between them: 201+202. The total change is divided 
+ * up over all compromised agents in all markets being attacked. 
  */
-public class Adv_Elvira extends Adversary{
+public class Adv_Elvira extends Adversary {
 
-    Demand falseBid = new Demand();
-    
+    private int eachShift;
+    private boolean do_attack;
+
+    /**
+     * Private class for information used in forging messages
+     */
+    static class ForgeInfo {
+       int from_id;
+       int to_id;
+       int tier;
+       boolean sup_con;
+       Channel channel;
+
+       ForgeInfo(int from, int to) {
+          this.from_id = from;
+          this.to_id = to;
+          this.sup_con = false;
+          this.channel = null;
+       }
+    }
+
+    /**
+     * List of ForgeInfo objects, one for each victim
+     */
+    private final ArrayList<ForgeInfo> forgeList = new ArrayList<>();
+          
     /**
      * Constructor
+     * 
+     * @param own_id Agent's id
      */
     public Adv_Elvira(int own_id) {
         super(own_id);
-        falseBid.add(1,-1000,1000);
     }
-    
-    /** 
+
+    /**
      * Initialize for a new population
      */
-    @Override 
+    @Override
     public void popInit() {
         super.popInit();
     }
 
-    /** 
+    /**
      * Reset at the beginning of a DOS run
      */
     @Override
     public void runInit() {
         super.runInit();
+
+        String[] targets ;
+        int targetId;
+        int shift;
+        Intel targetIntel;
+        History targetHistory;
+        String targetConstr;
+        ForgeInfo fi;
+
+        // 
+        //  Figure out how large the total attack should be and which 
+        //  markets we'd like to attack. Throw an exception if there
+        //  aren't any target markets.
+        //
+
+        shift   = Integer.parseInt(getConfig("shift"));
+        targets = getConfig("target").split("\\+");
+       
+        assert targets.length > 0 ;
+
+        //
+        //  Now look through all of the target markets and see which 
+        //  child nodes are compromised. Result will be a list indicating
+        //  which demand messages will be forged.
+        //
+
+        forgeList.clear() ;
+
+        for( String targetStr: targets ) {
+                    
+            // 
+            //  Look in history to see if flows into or out of the market 
+            //  were constrained. Only attack if they were.
+            //
+
+            targetId      = Integer.parseInt(targetStr);
+            targetIntel   = getIntel(targetId);
+            targetHistory = targetIntel.history;
+            targetConstr  = targetHistory.getConstr(period);
+
+            do_attack = targetConstr.equals("D") | targetConstr.equals("S") ;
+            
+            if( ! do_attack )
+               continue;
+
+            //
+            //  Ok, we're attacking. Build a list of compromised traders
+            //  that have the target market as their parent. While we're at 
+            //  it, divert messages from the victims. Also, set the trigger
+            //  tier to be one level up from the victim, since the forgery
+            //  will happen at the market tier.
+            //
+        
+            for (Intel i : intel.values()) {
+                if (i.compromised) {
+                    fi = new ForgeInfo(i.agent_id,i.par_id);
+                    fi.sup_con = targetConstr.equals("S");
+                    fi.tier    = i.tier + 1 ;
+                    fi.channel = Env.getAgent(i.agent_id).channel;
+                    fi.channel.divert_from(i.agent_id,this.own_id);
+                    forgeList.add(fi);
+                }
+            }
+        }
+        
+        //
+        //  Now divide the shock into parts based on the number of agents
+        //  whose demands will be forged. 
+        //
+
+        if( forgeList.size() > 0 )
+            eachShift = Math.round( (float) shift / forgeList.size());
     }
-    
+
     /**
      * Actions based on current simulation step
      *
@@ -44,104 +147,85 @@ public class Adv_Elvira extends Adversary{
      */
     @Override
     public void step(SimState state) {
-
+        int thisShift;
+        
         switch (Env.stageNow) {
 
         case PRE_AGGREGATE:
-            int period = 1;
-            int shift = Integer.parseInt(config.get("shift"));
-            int targetId = Integer.parseInt(config.get("target"));
-
-            Intel targetIntel = getIntel(targetId);
-            int targetPrice = targetIntel.history.p.get(period);
-            int targetCost = targetIntel.cost;
-            int targetCap = targetIntel.cap;
-            History targetHistory = targetIntel.history;
-
-            // Hashmap to store compromised agents' ids
-            HashMap<Integer, ArrayList<Integer>> compromisedAgents = new HashMap<>();
-            int count = 0;
-            int eachShift = 0;
-            for (Map.Entry<Integer, Intel> entry : intel.entrySet()) {
-                Intel i = entry.getValue();
-
-                if (i.compromised) {
-                    ++ count;
-                    if (compromisedAgents.get(i.par_id) == null) 
-                        compromisedAgents.put(i.par_id, new ArrayList<Integer>(Arrays.asList(i.agent_id)));
-                    else
-                        compromisedAgents.get(i.par_id).add(i.agent_id);
-
-                }
-            }
-            eachShift = Math.round(shift / count);
-
-            /*
-            System.out.println(eachShift);
-
-            for (Map.Entry<Integer, ArrayList<Integer>> entry : compromisedAgents.entrySet()) {
-                System.out.println(entry.getKey() + ":");
-                for (Integer id : entry.getValue()) {
-                    System.out.println(id);
-                }
-            }
-            */
-
-            if (targetHistory.getConstr(period).equals("D")) {
-                System.out.println("Attack Triggered!");
-
-                // Generate false bid
-                // TODO: check arraylist
-                eachShift = Math.round(shift / compromisedAgents.get(targetId).size());
-                for (Integer traderId : compromisedAgents.get(targetId)) {
-                    Intel trader = getIntel(traderId);
-                    Demand fakeDemand = new Demand();
-
-                    for (int bidPrice : trader.history.upD.get(period).bids.keySet()) {
-                        fakeDemand.add(bidPrice,
-                                       trader.history.upD.get(period).getBid(bidPrice).q_min - eachShift,
-                                       trader.history.upD.get(period).getBid(bidPrice).q_max - eachShift);
-                    }
-
-                    Msg msg = new Msg(this, targetId);
-                    msg.setDemand(fakeDemand);
-                    Channel channel = Channel.find(trader.channel);
-                    assert channel != null;
-                    channel.send(msg);
-                }
-
-            } else if (targetHistory.getConstr(period).equals("S")) {
-                System.out.println("Attack Triggered!");
-
-                // Generate false bid
-                eachShift = Math.round(shift / compromisedAgents.get(targetId).size());
-                //System.out.println(eachShift);
-                for (Integer traderId : compromisedAgents.get(targetId)) {
-                    Intel trader = getIntel(traderId);
-                    //System.out.println(trader.agent_id);
-                    Demand fakeDemand = new Demand();
-
-                    for (int bidPrice : trader.history.upD.get(period).bids.keySet()) {
-                        fakeDemand.add(bidPrice,
-                                       trader.history.upD.get(period).getBid(bidPrice).q_min + eachShift,
-                                       trader.history.upD.get(period).getBid(bidPrice).q_max + eachShift);
-                    }
-
-                    Msg msg = new Msg(this, targetId);
-                    msg.setDemand(fakeDemand);
-                    Channel channel = Channel.find(trader.channel);
-                    assert channel != null;
-                    channel.send(msg);
-                }
-
-            } else {
-                System.out.println("No Attack Triggered!");
-            }
-
-
-            break;
             
-        default:
+            if( ! do_attack ) {
+                Util.debug("No attack: no constrained markets");
+                return;
+            }
+
+            if( forgeList.isEmpty() ) {
+                Util.debug("No attack: no vulnerable agents found");
+                return;
+            }
+            
+            //  
+            // OK, looks like we should try to corrupt things with some
+            // forged messages. Go through the list of victims and do
+            // the dirty work. 
+            //
+            
+            for( ForgeInfo fi: forgeList ) {
+                Integer traderId;
+                
+                //
+                // Avoid sending spurious messages by being careful to 
+                // inject messages at the tier after the originals were sent. 
+                //
+                
+                if( fi.tier != Env.curTier )
+                    continue;
+                
+                //
+                // Good to go: build and inject the message. Start by 
+                // recovering the previous net demand and figuring out
+                // which way to shift it.
+                //
+                
+                traderId          = fi.from_id ;
+                Intel traderIntel = getIntel(traderId);
+                Demand histDemand = traderIntel.history.upD.get(period);
+                
+                thisShift = eachShift;
+                if( fi.sup_con )thisShift = -eachShift;
+                
+                //
+                // Build the new demand
+                //
+                
+                Demand fakeDemand = new Demand();
+                for (int bidPrice : histDemand.bids.keySet()) {
+                    fakeDemand.add(bidPrice,
+                        histDemand.getBidMin(bidPrice) - thisShift,
+                        histDemand.getBidMax(bidPrice) - thisShift);
+                }
+
+                //
+                // Add it to the demand log file
+                //
+                
+                fakeDemand.log(Env.getAgent(traderId),Demand.Type.FAKE);
+
+                //
+                // Build and send the message; note that we're lying about
+                // the from address.
+                //
+                
+                Msg msg = new Msg(this, fi.to_id);
+                msg.setDemand(fakeDemand);
+                msg.setFrom(fi.from_id);
+
+                Util.debug("message injected to "+fi.to_id+
+                           " from "+this.own_id+
+                           " as "+fi.from_id+", shift "+thisShift);
+
+                fi.channel.inject(msg);
+            }
+            
             break;
         }
     }
